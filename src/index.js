79489715a09f2a968939b110ad62fee03ce9600e
Matthew Cheely <matthew.cheely@genesys.com>
2018-05-08 22:22:09 -0400

    Use url in component route.

--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ import './libs/coordinatorLib.js';
 
 document.getElementById('router').registerComponents({
     "component1": {
-        "indexPath": "/components/example1/",
+        "url": "//components/example1/",
         "assignedRoute": "/one"
     },
     "component2": {
-        "indexPath": "/components/example2/",
+        "url": "//components/example2/",
         "assignedRoute": "/two"
+    },
+    "admin": {
+        "url": "https://localhost:4400/admin-v2/#/",
+        "assignedRoute": "/admin"
     }
 });
