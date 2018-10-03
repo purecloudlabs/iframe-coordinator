90d6abd3e735c9a44fce0417e566f4958411ad04
Jarrod Stormo <stormojm@gmail.com>
2018-10-03 17:33:34 +0200

    Adding better types for the Elm modules

--- js/iframe-coordinator/libs/types.ts
+++ js/iframe-coordinator/libs/types.ts
@@ -1,4 +1,20 @@
-export interface LabeledMsg {
-  msgType: string;
-  msg: any;
+export interface Location {
+  href: string;
+  host: string;
+  hostname: string;
+  protocol: string;
+  origin: string;
+  port: string;
+  pathname: string;
+  search: string;
+  hash: string;
+  username: string;
+  password: string;
+}
+
+// For some reason, HTMLAnchorElement doesn't define username and password
+// TODO: verify username/password support on anchors across browsers.
+export interface WorkaroundAnchor extends HTMLAnchorElement {
+  username: string;
+  password: string;
 }
