b68353ce3308c51175527ed8de29f5da5155766c
Matthew Cheely <matthew.cheely@genesys.com>
2018-10-04 15:09:41 -0400

    Prepare library for publication.
    Publication to the elm package system gives us enforced semver, but
    there are other requirements to meet as well, like having docs and
    making sure the code doesn't rely on the Debug module, which can't be
    used in an optimised build.

--- src/Message/AppToClient.elm
+++ src/Message/AppToClient.elm
@@ -1,5 +1,13 @@
 module Message.AppToClient exposing (AppToClient(..), decodeFromApp)
 
+{-| This module exposes a type representing all of the messages that can be
+passed from a client application to the client library. It's a fairly direct
+mapping to the client-facing API.
+
+@docs AppToClient, decodeFromApp
+
+-}
+
 import Dict
 import Json.Decode as Decode exposing (Decoder)
 import LabeledMessage
@@ -8,6 +16,10 @@ import Message.PubSub as PubSub exposing (Publication)
 import Message.Toast as Toast exposing (Toast)
 
 
+{-| This custom type is just a set of variants for each valid message the app
+can send to the client library. See the individual type descriptions for more
+details on message structures.
+-}
 type AppToClient
     = NavRequest Navigation
     | Publish Publication
@@ -16,6 +28,59 @@ type AppToClient
     | ToastRequest Toast
 
 
+{-| Decoder for parsing incoming messages from the application to the relevant
+types. Bad input from clients will produce nice console errors that describe the
+exact failure:
+
+    Could not parse incoming message: Json.Decode.oneOf failed in the following 5 ways:
+
+
+    (1) Problem with the given value:
+
+        {
+                "msgType": "toastRequest",
+                "msg": {
+                    "title": "Hello iframe World",
+                    "custom": {
+                        "level": "success"
+                    }
+                }
+            }
+
+        Unrecognized msg type: toastRequest
+
+
+
+    (2) Problem with the given value:
+
+        ...
+
+
+
+    (3) Problem with the given value:
+
+        ...
+
+
+
+    (4) Problem with the given value:
+
+        ...
+
+
+
+    (5) Problem with the value at json.msg:
+
+            {
+                "title": "Hello iframe World",
+                "custom": {
+                    "level": "success"
+                }
+            }
+
+        Expecting an OBJECT with a field named `message`: <internals>
+
+-}
 decodeFromApp : Decoder AppToClient
 decodeFromApp =
     Decode.oneOf
--- src/Message/AppToHost.elm
+++ src/Message/AppToHost.elm
@@ -1,5 +1,13 @@
 module Message.AppToHost exposing (AppToHost(..), decodeFromApp)
 
+{-| This module exposes a type representing all of the messages that can be
+passed from a host application to the host library. It's a fairly direct
+mapping to the host-facing API.
+
+@docs AppToHost, decodeFromApp
+
+-}
+
 import Dict
 import Json.Decode as Decode exposing (Decoder)
 import LabeledMessage exposing (expectLabel)
@@ -7,6 +15,10 @@ import Message.PubSub as PubSub exposing (Publication)
 import Path exposing (Path)
 
 
+{-| This custom type is just a set of variants for each valid message the app
+can send to the host library. See the individual type descriptions for more
+details on message structures.
+-}
 type AppToHost
     = Publish Publication
     | Subscribe String
@@ -14,6 +26,10 @@ type AppToHost
     | RouteChange Path
 
 
+{-| Decoder for parsing incoming messages from the application to the relevant
+types. Bad input from apps will produce nice console errors that describe the
+exact failure.
+-}
 decodeFromApp : Decoder AppToHost
 decodeFromApp =
     Decode.oneOf
