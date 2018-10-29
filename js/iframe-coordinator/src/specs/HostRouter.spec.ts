a40ce2fcdf0c6ae0c94d63eb3e1558124f899208
Jarrod Stormo <stormojm@gmail.com>
2018-10-29 15:17:29 +0100

    Adding tests for host router

--- js/iframe-coordinator/src/specs/HostRouter.spec.ts
+++ js/iframe-coordinator/src/specs/HostRouter.spec.ts
@@ -5,25 +5,29 @@ describe('HostRouter', () => {
   let mocks: any;
   let hostRouter: HostRouter;
   beforeEach(() => {
-    mocks = {
-      ifcFrame: {
-        default: class MockIFCFrame {
-          public setAttribute() {
-            // Empty
-          }
-
-          public addEventListener() {
-            // Empty
-          }
-        }
-      },
-      node: {
-        appendChild: () => {
-          // Empty
-        }
-      }
-    };
     /* tslint:disable */
+    mocks = {};
+    mocks.ifcFrameObj = {
+      handlers: {},
+      addEventListener: jasmine
+        .createSpy('xifcAddEventListener')
+        .and.callFake((topic: string, handler: (data: any) => void) => {
+          mocks.ifcFrameObj.handlers[topic] = handler;
+        }),
+      raise(topic: string, data: any) {
+        mocks.ifcFrameObj.handlers[topic](data);
+      },
+      setAttribute: jasmine.createSpy('xifcSetAttribute')
+    };
+    mocks.ifcFrame = {
+      default: jasmine
+        .createSpy('xifcConstructor')
+        .and.returnValue(mocks.ifcFrameObj)
+    };
+    mocks.node = {
+      appendChild: jasmine.createSpy('nodeAppendChild')
+    };
+
     const HostRouter = hostRouterInjector({
       './elements/x-ifc-frame': mocks.ifcFrame
     }).HostRouter;
@@ -40,7 +44,93 @@ describe('HostRouter', () => {
     });
   });
 
-  it('should create host routers', () => {
-    expect(hostRouter).not.toBe(undefined);
+  it('should create a new x-ifc-frame', () => {
+    expect(mocks.ifcFrame.default).toHaveBeenCalled();
+  });
+
+  it('should add the ifc-frame to the target node', () => {
+    expect(mocks.node.appendChild).toHaveBeenCalledWith(mocks.ifcFrameObj);
+  });
+
+  describe('when publishing a generic message to the client', () => {
+    let handlerData: LabeledMsg;
+    const message = {
+      msgType: 'test',
+      msg: 'test.data'
+    };
+    beforeEach(() => {
+      hostRouter.onSendToClient(data => {
+        handlerData = data;
+      });
+      hostRouter.publishGenericMessage(message);
+    });
+
+    it('shoud send the message to the client', () => {
+      expect(handlerData).toEqual(message);
+    });
+  });
+
+  describe('when client is sending message to host', () => {
+    describe('if the message is a public from the client', () => {
+      describe('if we are interested in the publication topic', () => {
+        let handlerData: LabeledMsg;
+        const incomingMessage = {
+          msgType: 'publish',
+          msg: {
+            topic: 'test.topic',
+            payload: 'test.payload'
+          }
+        };
+        beforeEach(() => {
+          hostRouter.subscribeToMessages('test.topic');
+          hostRouter.onSendToHost(data => {
+            handlerData = data;
+          });
+          mocks.ifcFrameObj.raise('clientMessage', {
+            detail: incomingMessage
+          });
+        });
+
+        it('should route the message to the host', () => {
+          expect(handlerData).toEqual(incomingMessage);
+        });
+      });
+
+      describe('if we are not interested in the publication topic', () => {
+        let handlerData: LabeledMsg;
+        const incomingMessage = {
+          msgType: 'publish',
+          msg: {
+            topic: 'test.topic',
+            payload: 'test.payload'
+          }
+        };
+        beforeEach(() => {
+          hostRouter.onSendToHost(data => {
+            handlerData = data;
+          });
+          mocks.ifcFrameObj.raise('clientMessage', {
+            detail: incomingMessage
+          });
+        });
+
+        it('should route the message to the host', () => {
+          expect(handlerData).toBeUndefined();
+        });
+      });
+    });
+  });
+
+  describe('when the route is changing', () => {
+    beforeEach(() => {
+      hostRouter.changeRoute('route/one');
+    });
+
+    it('should update the frame source location', () => {
+      expect(mocks.ifcFrameObj.setAttribute).toHaveBeenCalledWith(
+        'src',
+        '/test/one'
+      );
+    });
   });
 });
