322c991e3722e706aea384a05b7fc6c943cce38e
justin.ray <justin.ray@inin.com>
2018-11-02 17:47:34 -0400

    Add error rate tracking on workers Update demo for error rate tracking test

--- js/demo-app/public/workers/failure-worker.js
+++ js/demo-app/public/workers/failure-worker.js
@@ -1,5 +1,15 @@
-// This worker is designed to showcase error worker error handling.
-// The bug is intentional
-setTimeout(function () {
+// This worker is designed to showcase worker error rate handling.
+
+setTimeout(triggerError, getTimeout());
+
+function triggerError() {
+  // Setup the next error call, since the error will stop this function execution
+  setTimeout(triggerError, getTimeout());
+
+  console.error('Triggering an intentional error from a background worker.  Will not pop rate limit');
   foo.bar.baz();
-}, Math.max(5000, Math.random() * 10000));
+}
+
+function getTimeout(min=10000, max=30000) {
+  return Math.max(min, Math.round(Math.random() * max));
+}
--- js/iframe-coordinator/libs/workers/worker-manager.ts
+++ js/iframe-coordinator/libs/workers/worker-manager.ts
@@ -11,6 +11,9 @@ import * as SpawnWorker from './spawn-worker.worker.ts';
 
 export const WORKER_MESSAGE_EVENT_TYPE: string = 'workermessage';
 
+const DEFAULT_ERROR_WINDOW_COUNT_THRESHOLD = 10;
+const DEFAULT_ERROR_WINDOW_MILLIS = 30000;
+
 /*
  * TODO for this feature:
  *
@@ -20,17 +23,22 @@ export const WORKER_MESSAGE_EVENT_TYPE: string = 'workermessage';
  * Make sure it works in IE
  *  check blob and fallback
  * get bi-directional comms working
- * Deal with lenient shutdown?
  * Improve worker demo for the real, useful demo
  * Docs and tests
  *   dont forget that this is sorta dangerous with indexeddb
- * Handle errors from worker
- *  general (error rate?, allow restart)
+ *   tests for error rate
  * better logging (these are important)
  * decide if i need the protocol in the message payload or not.
  *  check it inbound if so
  *  remove it from outbound if not
  * stronger types of messages between this manager and the workers?
+ * support lenient shutdown?
+ *  notification on shutdown?
+ *  soft stop
+ *  allow restart?
+ * support client stop request?
+ *   worker never can close now as i have attached listeners
+ *   but, you can't re-add now either via registerclients
  */
 
 enum WorkerPhase {
@@ -47,6 +55,8 @@ interface ManagedWorker {
   url: string;
   phase: WorkerPhase;
   worker: Worker;
+  errorWindowCount: number;
+  errorWindowStartTimestamp: number;
 }
 
 export default class WorkerManager implements EventListenerObject, EventTarget {
@@ -55,8 +65,16 @@ export default class WorkerManager implements EventListenerObject, EventTarget {
   private static ID_PREFIX = 'iframeCoordinatorWorker-';
   private static _workerIndex = 0;
   private _workers: ManagedWorker[];
+  private _errorWindowCountThreshold = 0;
+  private _errorWindowMillis = 0;
 
-  constructor() {
+  constructor(
+    errorWindowCountThreshold: number = DEFAULT_ERROR_WINDOW_COUNT_THRESHOLD,
+    errorWindowMillis: number = DEFAULT_ERROR_WINDOW_MILLIS
+  ) {
+    this._errorWindowCountThreshold = errorWindowCountThreshold;
+    this._errorWindowMillis = errorWindowMillis;
+
     if (WorkerManager.evtTargetDelegate === null) {
       WorkerManager.evtTargetDelegate = document.createDocumentFragment();
     }
@@ -77,7 +95,9 @@ export default class WorkerManager implements EventListenerObject, EventTarget {
       id,
       url,
       worker,
-      phase: WorkerPhase.LOADING
+      phase: WorkerPhase.LOADING,
+      errorWindowCount: 0,
+      errorWindowStartTimestamp: -1
     });
 
     return id;
@@ -127,15 +147,42 @@ export default class WorkerManager implements EventListenerObject, EventTarget {
       });
 
       if (managedWorker) {
-        // TODO Should I be lenient when running?  max error rate?
-        // TODO allow restart?
-        // TODO Need to add proper logging support
-        // tslint:disable-next-line
-        console.error('Error occurred in worker.  Stopping the worker', {
-          workerDetails: managedWorker,
-          errorDetails: evt.error
-        });
-        this.stop(managedWorker.id);
+        const currentTimestamp = Date.now();
+
+        if (managedWorker.errorWindowStartTimestamp < 0) {
+          // First Error
+          managedWorker.errorWindowStartTimestamp = currentTimestamp;
+          managedWorker.errorWindowCount = 1;
+          return;
+        }
+
+        const delta = Math.max(
+          currentTimestamp - managedWorker.errorWindowStartTimestamp
+        );
+        if (delta > this._errorWindowMillis) {
+          // Window expired; start new window
+          managedWorker.errorWindowStartTimestamp = currentTimestamp;
+          managedWorker.errorWindowCount = 1;
+          return;
+        } else {
+          managedWorker.errorWindowCount++;
+
+          if (
+            managedWorker.errorWindowCount > this._errorWindowCountThreshold
+          ) {
+            // TODO Need to add proper logging support
+            // tslint:disable-next-line
+            console.error('Error rate exceeded.  Stopping the worker', {
+              workerDetails: managedWorker,
+              errorWindowCount: managedWorker.errorWindowCount,
+              errorWindowStartTimestamp:
+                managedWorker.errorWindowStartTimestamp,
+              errorWindowDurationMillis: delta,
+              errorDetails: evt.error
+            });
+            this.stop(managedWorker.id);
+          }
+        }
       }
     }
   }
