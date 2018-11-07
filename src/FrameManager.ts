8fbe6dd6cdef04e17a96fb3a07177a0d08a435f7
Matthew Cheely <matthew.cheely@genesys.com>
2018-11-07 16:31:44 -0500

    Add docs

--- src/FrameManager.ts
+++ src/FrameManager.ts
@@ -10,6 +10,12 @@ import {
 const IFRAME_STYLE =
   'position: absolute; top: 0; left: 0; width: 100%; height: 100%;';
 
+/**
+ * FrameManager is responsible for managing the state of the client iframe.
+ * It changes locations in the frame when requested, and handles proxying
+ * of postMessage events to and from the client, with verification of
+ * the expected client origin.
+ */
 class FrameManager {
   private _iframe: HTMLIFrameElement;
   private _frameLocation: string;
@@ -26,6 +32,15 @@ class FrameManager {
     this._iframe.setAttribute('sandbox', 'allow-scripts allow-same-origin');
   }
 
+  /**
+   * Navigates the wrapped iframe to the provided location in a
+   * way that does not affect the host application's history.
+   * This prevents strange behavior in the forward and back buttons
+   * in the browser in certain edge cases.
+   *
+   * @param newLocation The new location the iframe should show. If `null`,
+   * the frame will be directed to 'about:blank'.
+   */
   public setFrameLocation(newLocation: string | null) {
     this._frameLocation = newLocation || 'about:blank';
     if (this._iframe.contentWindow) {
@@ -39,6 +54,14 @@ class FrameManager {
     }
   }
 
+  /**
+   * Sends a message to the client frame via the postMessage API.
+   * If the message isn't structured correctly, or the client frame
+   * has navigated away from the expected origin, the message will
+   * not be sent.
+   *
+   * @param message The message to send.
+   */
   public sendToClient(message: HostToClient) {
     const clientOrigin = this._expectedClientOrigin();
     if (this._iframe.contentWindow) {
@@ -49,6 +72,13 @@ class FrameManager {
     }
   }
 
+  /**
+   * Registers a handler function that will be called when the client frame
+   * posts a message to the host. Invalid messages and messages from unexpected
+   * origins will not trigger the handler function.
+   *
+   * @param handler The handler function that processes messages
+   */
   public listenToMessages(handler: ((event: ClientToHost) => void)) {
     this._window.addEventListener('message', event => {
       const validated = validateIncoming(event.data);
@@ -62,6 +92,11 @@ class FrameManager {
     });
   }
 
+  /**
+   * Embeds the wrapped iframe in the provided element.
+   *
+   * @param parent The element to place the iframe inside.
+   */
   public embed(parent: HTMLElement) {
     parent.appendChild(this._iframe);
   }
