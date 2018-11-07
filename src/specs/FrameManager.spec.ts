4e6657c5ab45dbe3fe4ecc9a6bc186e636076982
Matthew Cheely <matthew.cheely@genesys.com>
2018-11-07 11:35:44 -0500

    finish frame tests

--- src/specs/FrameManager.spec.ts
+++ src/specs/FrameManager.spec.ts
@@ -13,7 +13,7 @@ describe('FrameManager', () => {
       createElement: jasmine
         .createSpy('docCreateElement')
         .and.callFake((element: string) => {
-          return mocks.mockFrame;
+          return mocks.frame;
         })
     };
 
@@ -23,29 +23,29 @@ describe('FrameManager', () => {
       addEventListener: jasmine
         .createSpy('frameAddEventListener')
         .and.callFake((topic: string, handler: (data: any) => void) => {
-          mocks.mockFrame.handlers[topic] = handler;
+          mocks.window.handlers[topic] = handler;
         }),
       raise(topic: string, data: any) {
-        mocks.mockFrame.handlers[topic](data);
+        mocks.window.handlers[topic](data);
       },
       location: {
         href: 'http://test.example.com/foo/'
       }
     };
 
-    mocks.mockFrameWindow = {
+    mocks.frameWindow = {
       location: {
         replace: jasmine.createSpy('frameLocationReplace')
       },
       postMessage: jasmine.createSpy('framePostMessage')
     };
 
-    mocks.mockFrame = {
+    mocks.frame = {
       setAttribute: jasmine.createSpy('frameSetAttribute'),
       load: () => {
-        mocks.mockFrame.contentWindow = mocks.mockFrameWindow;
-        if (mocks.mockFrame.onload) {
-          mocks.mockFrame.onload();
+        mocks.frame.contentWindow = mocks.frameWindow;
+        if (mocks.frame.onload) {
+          mocks.frame.onload();
         }
       }
     };
@@ -59,7 +59,7 @@ describe('FrameManager', () => {
 
   it('Creates a sandboxed iframe when created', () => {
     expect(mocks.document.createElement).toHaveBeenCalledWith('iframe');
-    expect(mocks.mockFrame.setAttribute).toHaveBeenCalledWith(
+    expect(mocks.frame.setAttribute).toHaveBeenCalledWith(
       'sandbox',
       'allow-scripts allow-same-origin'
     );
@@ -68,30 +68,34 @@ describe('FrameManager', () => {
   describe("Can be used to set the frame's location", () => {
     it('before the frame loads', () => {
       frameManager.setFrameLocation('http://example.com/');
-      mocks.mockFrame.load();
-      expect(
-        mocks.mockFrame.contentWindow.location.replace
-      ).toHaveBeenCalledWith('http://example.com/');
+      mocks.frame.load();
+      expect(mocks.frame.contentWindow.location.replace).toHaveBeenCalledWith(
+        'http://example.com/'
+      );
     });
 
     it('after the frame loads', () => {
-      mocks.mockFrame.load();
+      mocks.frame.load();
       frameManager.setFrameLocation('http://example.com/');
-      expect(
-        mocks.mockFrame.contentWindow.location.replace
-      ).toHaveBeenCalledWith('http://example.com/');
+      expect(mocks.frame.contentWindow.location.replace).toHaveBeenCalledWith(
+        'http://example.com/'
+      );
     });
 
     it('to an empty page', () => {
-      mocks.mockFrame.load();
+      mocks.frame.load();
       frameManager.setFrameLocation(null);
-      expect(
-        mocks.mockFrame.contentWindow.location.replace
-      ).toHaveBeenCalledWith('about:blank');
+      expect(mocks.frame.contentWindow.location.replace).toHaveBeenCalledWith(
+        'about:blank'
+      );
     });
   });
 
   describe('Can send messages to the client frame', () => {
+    beforeEach(() => {
+      mocks.frame.load();
+    });
+
     it('restricted by set location origin', () => {
       const message = {
         msgType: 'publish',
@@ -100,11 +104,10 @@ describe('FrameManager', () => {
           payload: {}
         }
       } as HostToClient;
-      mocks.mockFrame.load();
       frameManager.setFrameLocation('http://example.com:4040/foo/bar/baz/');
       frameManager.sendToClient(message);
 
-      expect(mocks.mockFrame.contentWindow.postMessage).toHaveBeenCalledWith(
+      expect(mocks.frame.contentWindow.postMessage).toHaveBeenCalledWith(
         message,
         'http://example.com:4040'
       );
@@ -118,20 +121,95 @@ describe('FrameManager', () => {
           payload: {}
         }
       } as HostToClient;
-      mocks.mockFrame.load();
       frameManager.setFrameLocation('/foo/bar/');
       frameManager.sendToClient(message);
 
-      expect(mocks.mockFrame.contentWindow.postMessage).toHaveBeenCalledWith(
+      expect(mocks.frame.contentWindow.postMessage).toHaveBeenCalledWith(
         message,
         'http://test.example.com'
       );
     });
   });
 
-  describe('Can be used to subscribe to post messages', () => {
-    it('which are bocked if from a different frame', () => {
-      expect(true).toBe(false);
+  describe('Can be used to subscribe to client messages', () => {
+    beforeEach(() => {
+      mocks.handler = jasmine.createSpy('messageHandler');
+      mocks.frame.load();
+      frameManager.setFrameLocation('http://example.com/');
+      mocks.messageEvent = {
+        origin: 'http://example.com',
+        source: mocks.frame.contentWindow,
+        data: {
+          msgType: 'publish',
+          msg: {
+            topic: 'test.topic',
+            payload: {}
+          }
+        }
+      };
+      frameManager.listenToMessages(mocks.handler);
     });
+
+    it('if the messages are sent correctly', () => {
+      mocks.window.raise('message', mocks.messageEvent);
+      expect(mocks.handler).toHaveBeenCalledWith(mocks.messageEvent.data);
+    });
+
+    it('which are blocked if sent from an unexpected origin', () => {
+      mocks.messageEvent.origin = 'http://evil.com';
+      mocks.window.raise('message', mocks.messageEvent);
+      expect(mocks.handler).not.toHaveBeenCalled();
+    });
+
+    it('which are blocked if sent from an unexpected frame', () => {
+      mocks.messageEvent.source = {};
+      mocks.window.raise('message', mocks.messageEvent);
+      expect(mocks.handler).not.toHaveBeenCalled();
+    });
+
+    it('which are blocked if sent with invalid data', () => {
+      mocks.messageEvent.data = { msgType: 'notValid', msg: {} };
+      mocks.window.raise('message', mocks.messageEvent);
+      expect(mocks.handler).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('Can send messages to clients', () => {
+    beforeEach(() => {
+      mocks.frame.load();
+      frameManager.setFrameLocation('http://example.com/');
+    });
+
+    it('if the message is valid', () => {
+      const message = {
+        msgType: 'publish',
+        msg: {
+          topic: 'test.topic',
+          payload: {}
+        }
+      } as HostToClient;
+
+      frameManager.sendToClient(message);
+      expect(mocks.frame.contentWindow.postMessage).toHaveBeenCalledWith(
+        message,
+        'http://example.com'
+      );
+    });
+
+    /*
+    it('but not if the message is invalid', () => {
+      const message = {
+        msgType: 'notAValidType',
+        msg: {}
+      } as HostToClient;
+      frameManager.sendToClient(message);
+      expect(mocks.frame.contentWindow.postMessage).not.toHaveBeenCalled();
+    });
+    */
+  });
+
+  it('Can be embeded the frame in another element', () => {
+    frameManager.embed(mocks.node);
+    expect(mocks.node.appendChild).toHaveBeenCalledWith(mocks.frame);
   });
 });
