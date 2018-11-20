78db2522f514b7659030fe199368427f5bf1edf9
Matthew Cheely <matthew.cheely@genesys.com>
2018-11-20 16:42:29 -0500

    Update build process to handle migration to TypeScript
    Also adds some better tooling for pushing back tags and commits from
    Jenkins.

--- scripts/jenkins.sh
+++ scripts/jenkins.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
-set +x
 set -e
 
 SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 PROJ_DIR="${SCRIPTS_DIR}/.."
-NPM_UTILS="${PROJ_DIR}/npm-utils"
+NPM_UTILS="${WORKSPACE}/npm-utils"
+
+# Check out the latest npm-utils
+rm -rf ${NPM_UTILS} && git clone --single-branch -b master --depth=1 git@bitbucket.org:inindca/npm-utils.git ${NPM_UTILS}
 
 cd ${PROJ_DIR}
 
-# Check out the latest npm-utils
-rm -rf ${NPM_UTILS} && git clone --depth=1 git@bitbucket.org:inindca/npm-utils.git ${NPM_UTILS}
-
-cd ${PROJ_DIR}/js/iframe-coordinator
-
 # Set up node with the provided version and generate a .npmrc file for our private npm repo
-source ${NPM_UTILS}/scripts/jenkins-pre-build.sh 8.11.4 -m
+source ${NPM_UTILS}/scripts/jenkins-pre-build.sh 10.13.0 -m
 
 ${SCRIPTS_DIR}/buildlibs.sh
 
@@ -22,4 +19,6 @@ ${SCRIPTS_DIR}/buildlibs.sh
   # This may have been added during npm install and pre-push package.
   rm -f ${PROJ_DIR}.git/hooks/pre-push
 
-${NPM_UTILS}/scripts/version-and-publish.sh -n
+PUBLISHED_VERSION=$(${NPM_UTILS}/scripts/version-and-publish.sh -n)
+
+printf "PUBLISHED_TAG=v${PUBLISHED_VERSION}" > ${WORKSPACE}/publish.properties
