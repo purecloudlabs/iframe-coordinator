a7ea93ff5101dd3ae0443101c068cd17b29d9ed1
Shane Bruggeman <shane.bruggeman@genesys.com>
2020-02-25 15:38:41 -0500

    remove string castings mentioned in feedback

--- src/HostRouter.ts
+++ src/HostRouter.ts
@@ -25,14 +25,10 @@ export class HostRouter {
    * @param route The route to lookup, such as '/foo/bar/baz'
    */
   public getClientTarget(route: string): ClientTarget | null {
-    const desiredRoute = route || '';
     let clientTarget: ClientTarget | null = null;
 
     this._clients.forEach(client => {
-      const clientRoute = matchAndStripPrefix(
-        desiredRoute,
-        client.assignedRoute
-      );
+      const clientRoute = matchAndStripPrefix(route, client.assignedRoute);
       if (clientRoute !== null) {
         clientTarget = {
           id: client.id,
