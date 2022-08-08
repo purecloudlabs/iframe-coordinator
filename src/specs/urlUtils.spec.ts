3df97a57372891d1e77226c4fbbad22aaccde22a
Daragh King <daragh.king@genesys.com>
2022-08-08 10:36:33 +0100

    fix(url-utils): joinRoutes now accounts for routes that normalize to an empty string
    joinRoutes now accounts for routes that normalize to an empty string
    
    COMUI-1131

---
+++ src/specs/urlUtils.spec.ts
@@ -0,0 +1,28 @@
+import {
+  joinRoutes,
+  normalizeRoute,
+  stripLeadingSlash,
+  stripLeadingSlashAndHashTag,
+  stripTrailingSlash
+} from '../urlUtils';
+
+describe('urlUtils', () => {
+  describe('joinRoutes', () => {
+    [
+      { input: ['', '', ''], expectedOutput: '' },
+      { input: ['test', 'test', 'test'], expectedOutput: 'test/test/test' },
+      {
+        input: ['/test/', '/test/', '/test/'],
+        expectedOutput: 'test/test/test'
+      },
+      { input: ['test', '', 'test'], expectedOutput: 'test/test' },
+      { input: ['/test/', '', '/test/'], expectedOutput: 'test/test' },
+      { input: ['test', '/', 'test'], expectedOutput: 'test/test' },
+      { input: ['/test/', '/', '/test/'], expectedOutput: 'test/test' }
+    ].forEach(({ input, expectedOutput }, index) => {
+      it(`should work as expected (${index + 1})`, () => {
+        expect(joinRoutes(...input)).toBe(expectedOutput);
+      });
+    });
+  });
+});
