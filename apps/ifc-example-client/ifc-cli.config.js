940719e1dc7a533401a0b7b9b66e9bda569ced53
Matthew Cheely <matthew.cheely@genesys.com>
2024-11-07 20:25:41 +1300

    feat(ifc-cli): add support for proxy configuration via a json file
    This adds support for ifc-cli proxying specified paths to a local dev server or other server to
    enable same-origin hosting features. Proxies are configured via a json file that maps paths to
    target URLs.

--- apps/ifc-example-client/ifc-cli.config.js
+++ apps/ifc-example-client/ifc-cli.config.js
@@ -4,24 +4,24 @@ module.exports = function (frameRouter) {
   frameRouter.clientConfig = {
     clients: {
       application1: {
-        url: `http://${hostname}:8080/client-app-1/#/`,
+        url: `/clients/client-app-1/#/`,
         assignedRoute: "/app1",
       },
       application2: {
         // We are removing the built-in proxy route example for now, as there are conflicts with vite and
         // we will be adding improvements to the proxy behavior in the near future
-        url: `http://${hostname}:8080/client-app-2/#/`,
+        url: `/clients/client-app-2/#/`,
         assignedRoute: "/app2",
-        allow: "camera http://localhost:8080;", // optional
+        allow: "camera self;", // optional
         sandbox: "allow-presentation allow-modals", // optional
         defaultTitle: "iframe Application 2 Example", // optional, but needed for accessibility
       },
       application3: {
         // We are removing the built-in proxy route example for now, as there are conflicts with vite and
         // we will be adding improvements to the proxy behavior in the near future
-        url: `http://${hostname}:8080/client-app-2/#/`,
+        url: `/clients/client-app-2/#/`,
         assignedRoute: "/app2/test",
-        allow: "camera http://localhost:8080;", // optional
+        allow: "camera self;", // optional
         sandbox: "allow-presentation allow-modals", // optional
         defaultTitle: "iframe Application 2 Example", // optional, but needed for accessibility
       },
