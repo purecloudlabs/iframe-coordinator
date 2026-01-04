f340beb2b98c01e3be809c5fa3eebb457751e6fd
Matt Cheely <matthew.cheely@genesys.com>
2026-01-05 09:16:46 +1300

    chore: Fix build failure due to automated commit message

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -3,5 +3,8 @@ export default {
   rules: {
     "subject-case": [2, "never", ["pascal-case", "upper-case"]],
   },
-  ignores: [(message) => message.includes("MIT")],
+  ignores: [
+    (message) => message.includes("MIT"),
+    (message) => message.includes("update license"),
+  ],
 };
