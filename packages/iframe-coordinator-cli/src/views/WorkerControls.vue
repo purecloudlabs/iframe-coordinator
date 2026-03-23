c6f1c96249942dee8da235016ed28ac39edcb37b
Daragh King <daragh.king@genesys.com>
2026-03-23 21:47:53 +0000

    refactor(prettier): fixed prettier issue
    fixed prettier issue

--- packages/iframe-coordinator-cli/src/views/WorkerControls.vue
+++ packages/iframe-coordinator-cli/src/views/WorkerControls.vue
@@ -5,22 +5,23 @@
       <select v-model="activeWorker" id="worker-select">
         <option
           v-for="(client, name) in workerPool.workerConfig.clients"
+          :key="name"
         >
-          {{name}}
+          {{ name }}
         </option>
       </select>
     </div>
     <div id="worker-communication">
       <div>
         <label for="message-topic">Topic:</label>
-        <input v-model="topic" type="text" id="message-topic"></input>
+        <input v-model="topic" type="text" id="message-topic" />
       </div>
       <div>
         <label for="worker-message">Message</label>
         <textarea v-model="message" id="worker-message"></textarea>
-      <div>
-      <button v-on:click="sendMessage()">Send to {{activeWorker}}</button>
-    <div>
+      </div>
+      <button v-on:click="sendMessage()">Send to {{ activeWorker }}</button>
+    </div>
   </div>
 </template>
 
@@ -29,8 +30,8 @@ let defaultMessage = {
   api: "requestNotification",
   data: {
     title: "Worker Notification",
-    message: "A worker sent this"
-  }
+    message: "A worker sent this",
+  },
 };
 
 export default {
@@ -38,47 +39,47 @@ export default {
   props: ["workerPool"],
   data() {
     return {
-      topic: 'exerciseApi',
+      topic: "exerciseApi",
       message: JSON.stringify(defaultMessage, null, 2),
-      activeWorker: Object.keys(this.workerPool.workerConfig.clients)[0]
+      activeWorker: Object.keys(this.workerPool.workerConfig.clients)[0],
     };
   },
   methods: {
-    sendMessage: function() {
+    sendMessage: function () {
       this.workerPool.publish(this.activeWorker, {
         topic: this.topic,
-        payload: JSON.parse(this.message)
+        payload: JSON.parse(this.message),
       });
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style>
-  #worker-control-panel {
-    width: 500px;
-    padding: 20px;
-    background-color: #33383d;
-    color: #fdfdfd;
-    text-align: left;
-  }
+#worker-control-panel {
+  width: 500px;
+  padding: 20px;
+  background-color: #33383d;
+  color: #fdfdfd;
+  text-align: left;
+}
 
-  #worker-selection {
-    text-align: center;
-    margin-bottom: 20px;  
-  }
+#worker-selection {
+  text-align: center;
+  margin-bottom: 20px;
+}
 
-  label[for="worker-select"] {
-    margin-right: 10px;
-  }
+label[for="worker-select"] {
+  margin-right: 10px;
+}
 
-  label[for="worker-message"] {
-    display: block;
-  }
+label[for="worker-message"] {
+  display: block;
+}
 
-  #worker-message {
-    width: 100%;
-    height: 200px;
-    margin-bottom: 5px;
-  }
+#worker-message {
+  width: 100%;
+  height: 200px;
+  margin-bottom: 5px;
+}
 </style>
